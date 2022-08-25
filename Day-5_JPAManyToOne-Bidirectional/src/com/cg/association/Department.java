5808c76b3186d7e52d87af9f18aaafb5dceeb933
admin <admin@DESKTOP-2U2EEDQ>
2022-08-25 15:36:03 +0530

    First Commit

---
+++ Day-5_JPAManyToOne-Bidirectional/src/com/cg/association/Department.java
@@ -0,0 +1,35 @@
+package com.cg.association;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="department")
+public class Department 
+{
+	@Id
+	
+	private int id;
+	private String name;
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
